276aba8b5f70ce86350d80a25b4f3156e8a17894
lfx <1512549743@qq.com>
2020-05-20 22:35:59 +0800

    创建springboot项目

---
+++ src/main/java/com/lfx/springboot/SpringbootGithubApplication.java
@@ -0,0 +1,13 @@
+package com.lfx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lfx/springboot/SpringbootGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lfx.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
